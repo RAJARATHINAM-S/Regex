d7ea38000af95befaea57192a58c6e0a3c68b939
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-10-06 10:17:07 +0530

    Regex E Mail is Added

---
+++ UC3_Valid_EMail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
+namespace Regex_Problem
+{
+    class UC3_Valid_EMail
+    {
+        public static string Regex_mail = "^[0-9a-zA-Z]+([.#_$+-][0-9a-zA-Z]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+        public UC3_Valid_EMail()
+        {
+            Console.WriteLine("Enter The EMail");
+
+        }
+        public bool Validate_Email(string email)
+        {
+            return Regex.IsMatch(email, Regex_mail);
+        }
+    }
+}
