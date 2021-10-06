c5e87d8851ad5507e9eefee7241de3ba386741b7
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-10-06 10:18:27 +0530

    Regex Phone Number Is Added

---
+++ UC4_PhoneNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Regex_Problem
+{
+    class UC4_PhoneNumber
+    {
+        public static string Regex_PhoneNumber = "^+([9][1]) ([0-9]{10,15})$";
+        public UC4_PhoneNumber()
+        {
+            Console.WriteLine("ENTER THE MOBILE NUMBER");
+        }
+        public bool Validate_PhoneNumber(string number)
+        {
+            return Regex.IsMatch(number, Regex_PhoneNumber);
+        }
+    }
+}
